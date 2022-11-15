5f867349f7316b671a16f8da8056fe2748c0fcb7
pageAccueil <yohaneendoke4@gmail.com>
2022-11-15 20:36:51 +0100

    Path commenté

--- Pokeproject/Pokeapp/urls.py
+++ Pokeproject/Pokeapp/urls.py
@@ -4,11 +4,9 @@ from requests import get
 from . import views
 
 urlpatterns = [
-    path('test/', views.test, name="test"),
     path('pokemons/', views.getPokemons, name="pokemonsList"),
     path('index/', views.setLanguage, name="language"),
-    path('pokemons/1/', views.getPokemonDetails, name="pokemonDetails"),
-    #path('pokemons/<id:id>/', views.getPokemonDetails(id=1), name="pokemonDetails"),
-    #path('pokemons/<int:id>/', views.getPokemonDetails(get, 1), name="pokemonDetails"),
-    path('', views.index, name= 'index'),
+    path('pokemons/<str:name>/', views.getPokemonDetails, name="pokemonDetails"),
+    path('pokemons/', views.search, name="searchPokemon"),
+    #path('', views.search, name="searchPokemon2"),
 ]
